f5d7e2031b512fb784114273a3828cd1cddcdbe5
JGutherie <jgutherie@microsoft.com>
2021-08-25 13:56:02 -0400

    Updated comments to document code

--- EventHubTriggerManagedIdentity/PeriodicSend.cs
+++ EventHubTriggerManagedIdentity/PeriodicSend.cs
@@ -1,10 +1,8 @@
 using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Azure.Identity;
 using Azure.Messaging.EventHubs;
 using Microsoft.Extensions.Logging;
-using System.Text;
 using System.Threading.Tasks;
 using Azure.Messaging.EventHubs.Producer;
 
@@ -18,17 +16,16 @@ namespace EventHubTriggerManagedIdentity
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
             try
-			{
-                //var credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = "16dde819-2b05-4fe7-b92b-6dcf0b8a1a7f" });
-                
+			{                
                 //Create a new producer client - leverage DefaultAzureCredentials to allowed Managed Identity to handle authentication and authorization
                 await using (var producer = new EventHubProducerClient(fullyQualifiedNamespace: Environment.GetEnvironmentVariable("eventHubConnection__fullyQualifiedNamespace"), eventHubName: Environment.GetEnvironmentVariable("eventHubName"), new DefaultAzureCredential()))
                 {
+                    //Create and send an event to the Event Hub
+                    
                     using EventDataBatch eventBatch = await producer.CreateBatchAsync();
                     eventBatch.TryAdd(new EventData(new BinaryData($"Event being sent at { DateTime.Now }")));
 
                     await producer.SendAsync(eventBatch);
-
                 }
             }
             catch(Exception ex) 
--- EventHubTriggerManagedIdentity/TestReceive.cs
+++ EventHubTriggerManagedIdentity/TestReceive.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Azure.Messaging.EventHubs;
-using Microsoft.Azure.WebJobs.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
@@ -12,18 +11,23 @@ namespace EventHubTriggerManagedIdentity
 {
     public static class TestReceive
     {
+        //Note the annotation for EventHubTrigger. The 'Connection' reference will actually pull a application configuration in the Function App Service named "eventHubConnection__fullyQualifiedNamespace"
+        //This is down to the newer version of the Microsoft.Azure.WebJobs SDK coupled with the v5.x.x-beta of Microsoft.Azure.WebJobs.Extensions.EventHubs being enabled for Managed Identities which no longer
+        //requires the connection string style used when leveraging SAS tokens. 
+        //Effectively, the new library is creating an EventHubConsumerClient from the configuration supplied in this annotation signature looking like:
+        //Azure.Messaging.EventHubs.Consumer.EventHubConsumerClient(string consumerGroupName, string fullyQualifiedNamespace, string eventHubName, Credential azureCredential)
         [FunctionName("HubReceiver")]
         public static async Task Run([EventHubTrigger("testhub", Connection = "eventHubConnection")] EventData[] events, ILogger log)
         {
             var exceptions = new List<Exception>();
-
+            
             foreach (EventData eventData in events)
             {
                 try
                 {
+                    //Type Azure.Messaging.EventHubs.EventData offers convenience accessors to the EventBody to do away with needing to use Encoding classes in System.Text directly
                     string messageBody = eventData.EventBody.ToString();
 
-                    // Replace these two lines with your processing logic.
                     log.LogInformation($"ATTENTION: INCOMING MESSAGE - C# Event Hub trigger function processed a message: {messageBody}");
                     await Task.Yield();
                 }
